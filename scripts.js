8f04ca4138e3b1c62a2a6f6ccb9e762c40f54314
Jana Jennings <jennings.jana@yahoo.com>
2022-11-01 21:16:34 +0900

    console game complete

--- scripts.js
+++ scripts.js
@@ -1,14 +1,46 @@
 function getComputerChoice() {
-    const choices = {1: "Rock", 2: "Scissors", 3:"Paper"};
+    const choices = {1: "rock", 2: "scissors", 3:"paper"};
     let choice = Math.floor(Math.random() * 3) + 1;
-    let computerChoice = choices[choice];
-    console.log(computerChoice);
+    const computerChoice = choices[choice];
     return computerChoice
     
 }
 
-getComputerChoice();
-
 function gameRound(computerChoice, playerChoice) {
-    computerChoice = getComputerChoice();
+    let win = `You won! ${playerChoice} beats ${computerChoice}`;
+    let lose = `You lost! ${computerChoice} beats ${playerChoice}`;
+    const draw = "It's a tie!";
+
+    if (computerChoice == playerChoice.toLowerCase()) {
+        return draw;
+    }
+    // rocks
+    if (computerChoice === "rock" && playerChoice.toLowerCase() === "paper") {
+        return win;
+    }
+    if (computerChoice === "rock" && playerChoice.toLowerCase() === "scissors") {
+        return lose;
+    }
+    // paper
+    if (computerChoice === "paper" && playerChoice.toLowerCase() === "rock") {
+        return lose;
+    }
+    if (computerChoice === "paper" && playerChoice.toLowerCase() === "scissors") {
+        return win;
+    }
+    //scissors
+    if (computerChoice === "scissors" && playerChoice.toLowerCase() === "paper") {
+        return lose;
+    }
+    if (computerChoice === "scissors" && playerChoice.toLowerCase() === "rock") {
+        return win;
+    }
 }
+
+const computerSelection = getComputerChoice();
+let playerChoice = prompt("What's your move?").toLowerCase();
+
+console.log(gameRound(computerSelection, playerChoice));
+
+
+
